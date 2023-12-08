9a846262ba54552f505dda10a5aa29ea841dcbaf
kripanshu mishra <92199408+kripanshumishra@users.noreply.github.com>
2023-12-08 16:35:31 +0530

    Update mod3.sol

--- mod3.sol
+++ mod3.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+
+contract OpenSkull is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("openSkull", "osk")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 10 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
 
